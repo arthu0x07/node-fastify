cae5629c611dd74dc2a19f446e69809cb6cc9385
Arthur Silva <arthur.santos.off@gmail.com>
2024-04-24 01:18:39 -0300

    chore: create eslint configs

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import fastify from "fastify";
-const app = fastify();
+import fastify from 'fastify'
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.get('/hello', () => {
+  return 'Hello World'
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("http server running!");
-  });
+    console.log('http server running!')
+  })
