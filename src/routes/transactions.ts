459c72c20eb55676cda3f4cf5c1e544670a6f1ed
Arthur Silva <arthur.santos.off@gmail.com>
2024-05-07 00:40:20 -0300

    feat: create a transactions sumary endpoint

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -22,6 +22,14 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return transaction
   })
 
+  app.get('/sumary', async () => {
+    const sumary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { sumary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
